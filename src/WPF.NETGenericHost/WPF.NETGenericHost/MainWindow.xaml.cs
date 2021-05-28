415b3a7f8ddb4c99021ad5aa0ccd5c1042f37602
Serajoddin Aliabadi <serajoddin.aliabadi@yahoo.com>
2021-05-28 18:53:32 +0430

    Use Logging in Main Window

--- src/WPF.NETGenericHost/WPF.NETGenericHost/MainWindow.xaml.cs
+++ src/WPF.NETGenericHost/WPF.NETGenericHost/MainWindow.xaml.cs
@@ -1,15 +1,18 @@
 ﻿using System.Windows;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace WPF.NETGenericHost
 {
     public partial class MainWindow : Window
     {
-        public MainWindow(IConfiguration configuration)
+        public MainWindow(IConfiguration configuration, ILogger<MainWindow> logger)
         {
             InitializeComponent();
 
             TextBox.Text = configuration.GetSection("MainWindowText").Value;
+
+            logger.LogCritical($"{nameof(MainWindow)} is Loaded!!!!");
         }
     }
 }
